8659df5cfa9b048c5d24f29f22d95ccbc3b413c4
CarloGrigioni <carlo.grigioni@student.supsi.ch>
2026-01-31 16:33:32 +0100

    chore: bump cache to v4 to force fixed css load

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getMessaging } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-sw.js";
 
-const CACHE_NAME = 'fantasfs-v3';
+const CACHE_NAME = 'fantasfs-v4';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
